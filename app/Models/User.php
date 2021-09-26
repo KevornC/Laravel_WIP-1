dbf654843e89da6584b625a6069bc96c375f82d1
Kevorn Callum <kevorn.callum16@gmail.com>
2021-09-26 18:27:56 -0500

    change models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    function StudentSelections(){
+        return $this->hasMany(StudentSelection::class,'user_id');
+    }
 }
